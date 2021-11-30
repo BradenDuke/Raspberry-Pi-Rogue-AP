73bfe6da05f870a5ba7b0b35e25317aa375db537
CJSantee <santeedev@gmail.com>
2021-11-30 12:37:52 -0600

    Updated auth script

--- nds_auth.sh
+++ nds_auth.sh
@@ -1,25 +1,65 @@
 #!/bin/sh
 
-METHOD="$1"
-MAC="$2"
+# Get the current Date/Time for the log
+date=$(date)
 
-case "$METHOD" in
-  auth_client)
-    USERNAME="$3"
-    PASSWORD="$4"
-    echo "Username: $USERNAME\nPassword: $PASSWORD" >> ~/Desktop/user_log.txt
-    ;;
-  client_auth|client_deauth|idle_deauth|timeout_deauth|ndsctl_auth|ndsctl_deauth|shutdown_deauth)
-    INGOING_BYTES="$3"
-    OUTGOING_BYTES="$4"
-    SESSION_START="$5"
-    SESSION_END="$6"
-    # client_auth: Client authenticated via this script.
-    # client_deauth: Client deauthenticated by the client via splash page.
-    # idle_deauth: Client was deauthenticated because of inactivity.
-    # timeout_deauth: Client was deauthenticated because the session timed out.
-    # ndsctl_auth: Client was authenticated by the ndsctl tool.
-    # ndsctl_deauth: Client was deauthenticated by the ndsctl tool.
-    # shutdown_deauth: Client was deauthenticated by Nodogsplash terminating.
-    ;;
-esac
+#
+# Get the action method from NDS ie the first command line argument.
+#
+# Possible values are:
+# "auth_client" - NDS requests validation of the client
+# "client_auth" - NDS has authorised the client
+# "client_deauth" - NDS has deauthorised the client
+# "idle_deauth" - NDS has deauthorised the client because the idle timeout duration has been exceeded
+# "timeout_deauth" - NDS has deauthorised the client because the session length duration has been exceeded
+# "ndsctl_auth" - NDS has authorised the client because of an ndsctl command
+# "ndsctl_deauth" - NDS has deauthorised the client because of an ndsctl command
+# "shutdown_deauth" - NDS has deauthorised the client because it received a shutdown command
+#
+action=$1
+
+if [ $action == "auth_client" ]; then
+       #
+       # The redir parameter is sent to this script as the fifth command line argument in url-encoded form.
+       #
+       # In the case of a simple splash.html login, redir is the URL originally requested by the client CPD.
+       #
+       # In the case of PreAuth or FAS it MAY contain not only the originally requested URL
+       # but also a payload of custom variables defined by Preauth or FAS.
+       #
+       # It may just be simply url-encoded (fas_secure_enabled 0 and 1), or
+       # aes encrypted (fas_secure_enabled 2)
+       #
+       # The username and password variables may be passed from splash.html, FAS or PreAuth and can be used
+       # not just as "username" and "password" but also as general purpose string variables to pass information to BinAuth.
+       #
+       # The client User Agent string is sent as the sixth command line argument.
+       # This can be used to determine much information about the capabilities of the client.
+       # In this case it will be added to the log.
+       #
+       # Both redir and useragent are url-encoded, so decode:
+       redir_enc=$5
+       redir=$(printf "${redir_enc//%/\\x}")
+       useragent_enc=$6
+       useragent=$(printf "${useragent_enc//%/\\x}")
+
+       # Append to the log.
+
+       echo "$date, method=$1, clientmac=$2, clientip=$7, username=$3, password=$4, redir=$redir, useragent=$useragent" >> /tmp/binauth.log
+else
+       echo "$date, method=$1, clientmac=$2, bytes_incoming=$3, bytes_outgoing=$4, session_start=$5, session_end=$6" >> /tmp/binauth.log
+fi
+
+
+# Set length of session in seconds (eg 24 hours is 86400 seconds - if set to 0 then defaults to global sessiontimeout value):
+session_length=0
+# The session length could be determined by FAS or PreAuth, on a per client basis, and embedded in the redir variable payload.
+
+# Finally before exiting, output the session length, followed by two integers (reserved for future use in traffic shaping)
+
+echo $session_length 0 0
+
+# exit 0 tells NDS is is ok to allow the client to have access.
+# exit 1 would tell NDS to deny access.
+
+exit 0
